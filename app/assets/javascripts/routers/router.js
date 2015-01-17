1f963ed8d255960bc40cd802bb35456ea25b5a5e
Tim Sandberg <tasandberg@gmail.com>
2015-01-16 19:25:44 -0800

    Fix projectscontroller, place under API dir

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -2,31 +2,16 @@ ProjectBoard.Routers.Router = Backbone.Router.extend({
   routes: {
     "" : "home",
   },
-	
+
 	initialize: function() {
 		this.$rootEl = $("#main-wrapper");
 		window.currentUser = new ProjectBoard.Models.User({id: CURRENT_USER});
-		currentUser.fetch({
-			success: function() {
-				window.projects = new ProjectBoard.Collections.Projects({
-					user: currentUser
-				});
-				projects.fetch({
-					wait: true,
-					success: function() {
-						_.each(this.projects.models, function(project) {
-							$('#main-wrapper').append("<h4>" + project.get('name') + '</h4>');
-						});
-					}
-				});
-			}
-		});
+		currentUser.fetch();
 	},
-	
+
 	home: function() {
 		console.log('home');
 	}
-	
-	
-});
 
+
+});
