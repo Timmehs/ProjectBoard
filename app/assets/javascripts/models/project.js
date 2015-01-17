aaea4b0da9715f01d0e69826d9b4fc372a7f0961
Tim Sandberg <tasandberg@gmail.com>
2015-01-17 00:23:34 -0800

    Project index is working

--- app/assets/javascripts/models/project.js
+++ app/assets/javascripts/models/project.js
@@ -1,3 +1,20 @@
 ProjectBoard.Models.Project = Backbone.Model.extend({
-	urlRoot: "/projects"
+	urlRoot: "/projects",
+
+	pbScore: function() {
+
+		// TODO: devise calculation of project_board score for project
+		// based on user 'karma' and project stats
+
+		Math.floor(Math.random() * 100);
+	},
+
+	authorGithub: function() {
+		return "http://github.com/" + this.authorName();
+	},
+
+	authorName: function() {
+		return this.get('author').username;
+	}
+
 });
