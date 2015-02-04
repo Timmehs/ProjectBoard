a50f93a2f1598ca8f29109515d5caf27474306f2
Tim Sandberg <tasandberg@gmail.com>
2015-02-04 13:47:41 -0800

    Add boards routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback" => "api/sessions#create"
 
   namespace :api, defaults: { format: :json } do
+    resources :boards, only: [:create, :index, :update, :destroy]
     resource :sessions, only: [:signin, :create, :destroy]
     resources :users, only: [ :index, :show ]
     resources :projects, only: [:index, :create, :destroy]
